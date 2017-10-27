506c1957b352817d9c18208a6f6e75006e1f8f60
Brian <karabinchak.brian@gmail.com>
2017-10-26 21:30:02 -0400

    Completed first assignment in Udemy Angular course

---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'warning-component',
+	templateUrl: './warning.component.html',
+	styleUrls:['./warning.component.css']
+})
+
+export class WarningComponent {
+
+}
